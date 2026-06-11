f970a339b930bdc713ececb7c612c88fbb01554e
sachin rana <90305111+Sachinrana89@users.noreply.github.com>
2026-06-12 00:06:10 +0530

    Add files via upload

---
+++ postive_numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum=0;
+    cout<<"enter a number";
+    cin>>n;
+    
+    while(n>=0)
+    {
+        sum = sum+n;
+        cout<<"enter a number";
+        cin>>n;
+    }
+    cout<<"the sum is "<<sum<<endl;
+    
+    return 0;
+}

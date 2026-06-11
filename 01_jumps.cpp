1e4fcf85600c199fb2574c76a0a6a36304da5920
sachin rana <90305111+Sachinrana89@users.noreply.github.com>
2026-06-12 00:06:41 +0530

    Add files via upload

---
+++ 01_jumps.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int pocket_money=300;
+    for(int date = 1; date<=30; date++)
+    {
+        if(date%2==0)
+        {
+            continue;
+        }
+        if(pocket_money==0)
+        {
+            break;
+        
+        }
+        cout<<"go out today : "<<date<<endl;
+        pocket_money=pocket_money-300;
+    }
+return 0;
+}

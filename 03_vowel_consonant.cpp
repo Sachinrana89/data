6b4a5565eaae1891530e18ff361d49fde1a27950
sachin rana <90305111+Sachinrana89@users.noreply.github.com>
2026-06-13 23:58:54 +0530

    Add files via upload

---
+++ 03_vowel_consonant.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout<<"enter a alphabat";
+    cin>>ch;
+
+    switch (ch)
+    {
+    case 'a':
+    cout<<"it is a vowel";
+    break;
+    
+    case 'e':
+    cout<<"it isa vowel";
+    break;
+
+    case 'i':
+    cout<<"it is a vowel";
+    break;
+
+    case 'o':
+    cout<<"it is a vowel";
+    break;
+
+    case 'u':
+    cout<<"it is a vowel";
+    break;
+    
+    case 'A':
+    cout<<"it is a vowel";
+    break;
+
+    case 'E':
+    cout<<"it is a vowel";
+    break;
+
+    case 'I':
+    cout<<"it is a vowel";
+    break;
+
+
+    case 'U':
+    cout<<"it is a vowel";
+    break;
+
+    case 'O':
+    cout<<"it is a vowel";
+    break;
+    default:
+    cout<<"it is a consonant";
+    break;
+}
+return 0;
+}

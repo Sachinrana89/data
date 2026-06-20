3eef7ad105c977f4f55827ef7b9c24c36a18c5ef
sachin rana <90305111+Sachinrana89@users.noreply.github.com>
2026-06-20 06:34:17 +0530

    Add files via upload

---
+++ 03_inverted_star.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(n; n>=1; --n)
+    {
+        for(int i = 1; i<=n; ++i)
+        cout<<"*";
+        cout<<endl;
+    }
+    return 0;
+}
